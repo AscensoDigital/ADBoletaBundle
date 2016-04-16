6d20a52dc137b797e64e36e569ce9758fe6629cc
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-04-16 13:29:24 -0300

    se agrega carga de archivos de configuracion

---
+++ DependencyInjection/ADBoletaExtension.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace AscensoDigital\BoletaBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Created by PhpStorm.
+ * User: claudio
+ * Date: 20-01-16
+ * Time: 19:18
+ */
+class ADBoletaExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        /*$config = $this->processConfiguration(new Configuration(), $configs);
+        $allConfig=$this->loadBundleFiltros($config);
+        $container->setParameter('ad_perfil.config', $allConfig);
+
+        $container->setParameter('ad_perfil.perfil_class',$config['perfil_class']);
+        $container->setParameter('ad_perfil.session_name',$config['session_name']);
+        $container->setParameter('ad_perfil.route_redirect',$config['route_redirect']);
+
+        $container->setParameter('ad_perfil.navegacion.homepage_route',$config['navegacion']['homepage_route']);
+        $container->setParameter('ad_perfil.navegacion.homepage_name',$config['navegacion']['homepage_name']);
+        $container->setParameter('ad_perfil.navegacion.homepage_icono',$config['navegacion']['homepage_icono']);
+        $container->setParameter('ad_perfil.navegacion.homepage_color',$config['navegacion']['homepage_color']);
+        $container->setParameter('ad_perfil.navegacion.homepage_title',$config['navegacion']['homepage_title']);
+        $container->setParameter('ad_perfil.navegacion.homepage_subtitle',$config['navegacion']['homepage_subtitle']);
+*/
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+        $loader->load('doctrine.yml');
+    }
+
+    public function getAlias()
+    {
+        return 'ad_boleta';
+    }
+
+    private function loadBundleFiltros($config){
+        $filtro_permiso=[
+            'type' => 'Symfony\Bridge\Doctrine\Form\Type\EntityType',
+            'table_alias' => 'adp_prm',
+            'field' => 'id',
+            'operator' => 'in',
+            'query_builder_perfil' => false,
+            'query_builder_user' => false,
+            'query_builder_method' => 'getQueryBuilderOrderNombre',
+            'options' => [
+                'label' => 'Permiso',
+                'class' => 'AscensoDigital\PerfilBundle\Entity\Permiso',
+                'multiple' => true
+            ]];
+        $config['filtros']['adperfil_permiso']=$filtro_permiso;
+
+        $filtro_perfil=[
+            'type' => 'Symfony\Bridge\Doctrine\Form\Type\EntityType',
+            'table_alias' => $config['perfil_table_alias'],
+            'field' => 'id',
+            'operator' => 'in',
+            'query_builder_perfil' => true,
+            'query_builder_user' => false,
+            'query_builder_method' => 'getQueryBuilderOrderRole',
+            'options' => [
+                'label' => 'Perfil',
+                'class' => $config['perfil_class'],
+                'multiple' => true
+            ]];
+        $config['filtros']['adperfil_perfil']=$filtro_perfil;
+        return $config;
+    }
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace AscensoDigital\BoletaBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ad_boleta');
+
+        /*$rootNode
+            ->children()
+                ->scalarNode('perfil_class')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('perfil_table_alias')->isRequired()->cannotBeEmpty()->end()
+                ->scalarNode('route_redirect')->defaultValue('homepage')->end()
+                ->scalarNode('session_name')->defaultValue('ad_perfil.perfil_id')->end()
+                ->scalarNode('icon_path')->isRequired()->end()
+                ->scalarNode('icon_alt')->isRequired()->end()
+                ->arrayNode('navegacion')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('homepage_title')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('homepage_subtitle')->defaultValue('')->end()
+                        ->scalarNode('homepage_route')->defaultValue('homepage')->cannotBeEmpty()->end()
+                        ->scalarNode('homepage_name')->defaultValue('Inicio')->cannotBeEmpty()->end()
+                        ->scalarNode('homepage_icono')->defaultValue('fa fa-home')->cannotBeEmpty()->end()
+                        ->scalarNode('homepage_color')->defaultValue('blanco')->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ->end();
+
+        $this->addFiltroSection($rootNode);*/
+
+        return $treeBuilder;
+    }
+
+    private function addFiltroSection(ArrayNodeDefinition $rootNode) {
+        $rootNode
+            ->fixXmlConfig('filtro')
+            ->children()
+                ->arrayNode('filtros')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                    ->children()
+                        ->scalarNode('type')
+                            ->defaultValue('Symfony\Bridge\Doctrine\Form\Type\EntityType')
+                            ->cannotBeEmpty()
+                            ->info('Clase del tipo de form que será el filtro.')
+                            ->example('Symfony\Bridge\Doctrine\Form\Type\EntityType (see http://symfony.com/doc/current/reference/forms/types.html)')
+                        ->end()
+                        ->variableNode('table_alias')
+                            ->cannotBeEmpty()
+                            ->info('String o Array con Alias de la(s) "entity" usada(s) para filtrar')
+                            ->example('eg: para entity "Pais" alias "p"')
+                        ->end()
+                        ->scalarNode('field')
+                            ->defaultValue('id')
+                            ->cannotBeEmpty()
+                            ->info('Nombre del "field" que se filtra de la tabla con "table_alias"')
+                            ->example('eg: id')
+                        ->end()
+                        ->scalarNode('operator')
+                            ->defaultValue('in')
+                            ->cannotBeEmpty()
+                            ->info('Valor para el operador de comparación')
+                            ->example('eg para equal: eq (see http://www.doctrine-project.org/api/orm/2.5/class-Doctrine.ORM.Query.Expr.html)')
+                        ->end()
+                        ->scalarNode('function')
+                            ->info('Funcion SQL a aplicar')
+                            ->example('eg: date')
+                        ->end()
+                        ->scalarNode('query_builder_method')
+                            ->info('Nombre del metodo usado para generar la opción query_builder')
+                            ->example('eg: getQueryBuilderFindAll')
+                        ->end()
+                        ->booleanNode('query_builder_perfil')
+                            ->defaultFalse()
+                            ->info('Determina si al query_builder se le pasa como parámetro el objeto perfil')
+                        ->end()
+                        ->booleanNode('query_builder_user')
+                            ->defaultFalse()
+                            ->info('Determina si al query_builder se le pasa como parámetro el objeto user')
+                        ->end()
+                        ->arrayNode('options')
+                            ->info('Lista de opciones según "type" del filtro.')
+                            ->prototype('variable')
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ->end();
+    }
+}
