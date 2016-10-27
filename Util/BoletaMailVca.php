fbc8e2fb36d8a0066cedc61a34b6780f438df3c3
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-10-27 01:44:58 -0300

    se reescribe funcion para obtener la razon social

--- Util/BoletaMailVca.php
+++ Util/BoletaMailVca.php
@@ -14,4 +14,16 @@ class BoletaMailVca extends BoletaMailAnulada {
     public static function loadMsg($msg){
         parent::loadMsg($msg);
     }
+
+    public static function getRazonSocial(){
+        if(is_null(self::$razonSocial)) {
+            $i=strpos(self::$msg,'receptor  ');
+            if(false!==$i){
+                $init = $i + strlen('receptor  ');
+                $final=strpos(self::$msg,',',$init+1);
+                self::$razonSocial=substr(self::$msg,$init,$final-$init);
+            }
+        }
+        return self::$razonSocial;
+    }
 }
