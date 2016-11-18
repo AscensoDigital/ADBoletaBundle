91d6f3f0eff4454dc43b5eed918bac07c7fe3dd8
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-11-18 00:01:15 -0300

    se cambia captura de la fecha

--- Util/CargaResumenBoletasSii.php
+++ Util/CargaResumenBoletasSii.php
@@ -89,7 +89,7 @@ class CargaResumenBoletasSii
                 $estado = "" != self::BH_ESTADO ? $sheet->getCell(self::BH_ESTADO . $row)->getValue() : null;
                 $fecha = "" != self::BH_FECHA ? $sheet->getCell(self::BH_FECHA . $row)->getFormattedValue() : null;
                 $fechaArr=explode('-',$fecha);dump($fecha);dump($fechaArr);
-                $datetime=new \DateTime('20'.$fechaArr[2].'-'.$fechaArr[1].'-'.$fechaArr[0]);
+                $datetime=new \DateTime('20'.$fechaArr[2].'-'.$fechaArr[0].'-'.$fechaArr[1]);
                 $rut = "" != self::EM_RUT ? $sheet->getCell(self::EM_RUT . $row)->getValue() : null;
                 $m_bruto = "" != self::MONTO_BRUTO ? $sheet->getCell(self::MONTO_BRUTO . $row)->getValue() : null;
                 $m_retenido = "" != self::MONTO_RETENIDO ? $sheet->getCell(self::MONTO_RETENIDO . $row)->getValue() : null;
