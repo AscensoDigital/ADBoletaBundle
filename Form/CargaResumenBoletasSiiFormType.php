34b3adf2fbb53008ca46b3d097e8fc628061f612
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-11-17 23:17:18 -0300

    se agrega detalle de tipos para formulario

--- Form/CargaResumenBoletasSiiFormType.php
+++ Form/CargaResumenBoletasSiiFormType.php
@@ -2,7 +2,9 @@
 
 namespace AscensoDigital\BoletaBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -10,8 +12,10 @@ class CargaResumenBoletasSiiFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('empresa')
-            ->add('file');
+        $builder->add('empresa', EntityType::class, [
+            'class' => 'ADBoletaBundle:Empresa'
+        ])
+            ->add('file', FileType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
