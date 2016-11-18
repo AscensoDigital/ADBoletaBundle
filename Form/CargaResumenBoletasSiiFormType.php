810de1c6e7e8de98798b656d46a60164650be2dd
Claudio Corvalan <claudio.corvalan@ennea.cl>
2016-11-17 22:54:33 -0300

    se configura formulario para cargar

--- Form/CargaResumenBoletasSiiFormType.php
+++ Form/CargaResumenBoletasSiiFormType.php
@@ -10,16 +10,19 @@ class CargaResumenBoletasSiiFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
+        $builder->add('empresa')
+            ->add('file');
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-
+        $resolver->setDefaults([
+            'data_class' => 'AscensoDigital\BoletaBundle\Util\CargaResumenBoletasSii'
+        ]);
     }
 
     public function getName()
     {
-        return 'adboleta_bundle_carga_boleta_sii';
+        return 'adboleta_carga_boleta_sii';
     }
 }
